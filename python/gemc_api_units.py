972a0f28451aa523d9165c5de96f16b7ac94644f
jsgeorge68 <jsgeorge68@gmail.com>
2017-02-19 17:25:03 -0800

    Add files via upload

---
+++ python/gemc_api_units.py
@@ -0,0 +1,12 @@
+#=======================================
+#	gemc units definition
+#
+#	J. George, 2/19/2017
+#
+#	This file provides a location to store various unit conversions that might be handy for other routines.
+#	Almost all of the math function in the perl api math module are unnecessary here because they are already defined
+#	in python.  However, the units need a home as well as any specific math functions that may turn up.
+
+
+inches = 2.54
+
